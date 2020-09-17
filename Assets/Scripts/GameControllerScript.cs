b6ad508751ca09b978357ecee9e72d1fff063c60
ajilpappachan <ajilpappachan@gmail.com>
2020-09-18 00:59:06 +0530

    Added Inventory

---
+++ Assets/Scripts/GameControllerScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControllerScript : MonoBehaviour
+{
+    public GameObject PlantSelector;
+    public GameObject Inventory;
+    public GameObject InventoryList;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            PlantSelector.SetActive(!PlantSelector.activeSelf);
+        }
+        if(Input.GetKeyDown(KeyCode.I))
+        {
+            Inventory.SetActive(!Inventory.activeSelf);
+        }
+    }
+
+    public void AddInventoryItem(GameObject item)
+    {
+        Instantiate(item, InventoryList.transform);
+    }
+}
---
+++ Assets/Scripts/PlantObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SerializeField]
+[CreateAssetMenu(fileName ="Plant", menuName ="Scriptables/Plant", order =1)]
+public class PlantObject : ScriptableObject
+{
+    public GameObject Dirt;
+    public GameObject Plant;
+    public GameObject Fruit;
+    public GameObject Inventory;
+}
