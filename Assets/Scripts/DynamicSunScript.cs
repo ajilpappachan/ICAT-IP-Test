5fd403b736874a29d2d707f9251ade56e368ab1d
ajilpappachan <ajilpappachan@gmail.com>
2020-09-18 13:51:53 +0530

    Added Post Processing

--- Assets/Scripts/DynamicSunScript.cs
+++ Assets/Scripts/DynamicSunScript.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 [ExecuteAlways]
 public class DynamicSunScript : MonoBehaviour
@@ -8,6 +10,10 @@ public class DynamicSunScript : MonoBehaviour
     [SerializeField] private Light DirectionalLight;
     [SerializeField] private DynamicSunObject Preset;
     [SerializeField, Range(0, 24)] private float TimeOfDay;
+
+    public Volume volume;
+    private ColorAdjustments color;
+    private WhiteBalance whiteBalance;
 
     private void OnValidate()
     {
@@ -32,6 +38,12 @@ public class DynamicSunScript : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+        volume.profile.TryGet<ColorAdjustments>(out color);
+        volume.profile.TryGet<WhiteBalance>(out whiteBalance);
+    }
+
     private void UpdateLighting(float timePercent)
     {
         RenderSettings.ambientLight = Preset.AmbientColor.Evaluate(timePercent);
@@ -41,6 +53,20 @@ public class DynamicSunScript : MonoBehaviour
         {
             DirectionalLight.color = Preset.DirectionalColor.Evaluate(timePercent);
             DirectionalLight.transform.localRotation = Quaternion.Euler(new Vector3((timePercent * 360f) - 90f, 170f, 0f));
+        }
+    }
+
+    private void UpdatePostProcessing(float time)
+    {
+        if(time > 5.0f && time < 16.0f)
+        {
+            color.postExposure.value = Mathf.Lerp(color.postExposure.value, 0.5f, Time.deltaTime / 4);
+            whiteBalance.temperature.value = Mathf.Lerp(whiteBalance.temperature.value, 50f, Time.deltaTime / 4);
+        }
+        else
+        {
+            color.postExposure.value = Mathf.Lerp(color.postExposure.value, 1.5f, Time.deltaTime / 4);
+            whiteBalance.temperature.value = Mathf.Lerp(whiteBalance.temperature.value, -50f, Time.deltaTime / 4);
         }
     }
 
@@ -54,6 +80,7 @@ public class DynamicSunScript : MonoBehaviour
             TimeOfDay += Time.deltaTime / 2;
             TimeOfDay %= 24;
             UpdateLighting(TimeOfDay / 24f);
+            UpdatePostProcessing(TimeOfDay);
         }
         else
         {
